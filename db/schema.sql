f6bbca9e069e721e940847bb2bbb66dd6f2f2a06
Aidancm1 <aidancmorrow1@gmail.com>
2023-07-30 12:06:32 -0400

    updated seeds.sql

--- db/schema.sql
+++ db/schema.sql
@@ -17,7 +17,7 @@ CREATE TABLE roles (
 );
 
 
-CREATE TABLE employee (
+CREATE TABLE employees (
     id NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
--- db/seeds.sql
+++ db/seeds.sql
@@ -4,5 +4,17 @@ VALUES (001, "Engineering"),
         (003, "Legal")
 
 
-INSERT INTO roles (role_id, employees)
-VALUES 
+INSERT INTO roles (title_id, salary_id, department_id)
+VALUES     ("Salesperson", 80000, "Sales"),
+           ("Lead Engineer", 150000, "Engineering"),
+           ("Software Engineer", 120000, "Engineering"),
+           ("Account Manager", 160000, "Finance"),
+
+
+-- Not sure about role_id, and manager_id
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
+VALUES      ("John", "Doe", 1, 2)
+            ("Ashley", "Rodriguez", 2, 3)
+            ("Kevin", "Tupik", 3, 4)
+
+
